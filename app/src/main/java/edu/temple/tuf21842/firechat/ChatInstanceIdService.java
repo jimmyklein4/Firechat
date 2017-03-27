2089beb4e73b5388c12a3ed9dd5dedce2a255f14
Jimmy Klein <james.klein@temple.edu>
2017-03-27 15:21:40 -0400

    Adding messaging

---
+++ app/src/main/java/edu/temple/tuf21842/firechat/ChatInstanceIdService.java
@@ -0,0 +1,31 @@
+package edu.temple.tuf21842.firechat;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static com.google.android.gms.internal.zzs.TAG;
+
+/**
+ * Created by jimmy on 3/26/17.
+ */
+
+public class ChatInstanceIdService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token){
+        //TODO Implement
+    }
+}
